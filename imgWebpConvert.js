6ad0972796111111ef6ea562c1849ce48435b3d3
mokka86coffee <43421744+mokka86coffee@users.noreply.github.com>
2019-03-14 14:58:04 +0300

    Jpej/Png Converter

---
+++ imgWebpConvert.js
@@ -0,0 +1,28 @@
+//html > head
+//<script src="2019/js/webpjs.js"></script>
+
+var webp = require('webp-converter');
+var fs = require('fs');
+var path = require('path');
+const sharp = require('sharp');
+
+let promReadDir = (way) => new Promise(r=> fs.readdir( way, (err, items) => r(items) ) );
+let promFileStat = (file) => new Promise(r=> fs.stat( file, (err, stats) => r( +(stats["size"]/1000).toFixed() ) ) );
+let sharpImg = (file) => new Promise(r=> sharp(file).resize(1000).toFile('src/output.jpg', (err, info) => r(info) ) );
+let convertImg = (way, fileName, quality) => new Promise(r=> webp.cwebp(`${way}/output.jpg`,`dist/${fileName}.webp`, quality, (status,error) => r() ) );
+
+
+(async ()=>{
+    let way = path.resolve(`${__dirname}/src`);
+
+    let files = await promReadDir(way);
+        
+    for (file of files) {
+            let fileName = file.replace(/\.(jpg|png)$/g, '');
+            let fileSize = await promFileStat(`${way}/${file}`);
+
+            await sharpImg(`${way}/${file}`);
+
+            let quality = "-q 80";            
+    }
+})();
