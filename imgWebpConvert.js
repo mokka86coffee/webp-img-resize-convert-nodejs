281f42fa45d98a0f1c88d12fd7775ad1b7dcbce7
Evgeniy_Marushev <emarushev@eshe.studio>
2019-10-11 21:30:22 +0300

    png fixed

--- imgWebpConvert.js
+++ imgWebpConvert.js
@@ -9,7 +9,7 @@ const sharp = require('sharp');
 let promReadDir = (way) => new Promise(r=> fs.readdir( way, (err, items) => r(items) ) );
 let promFileStat = (file) => new Promise(r=> fs.stat( file, (err, stats) => r( +(stats["size"]/1000).toFixed() ) ) );
 let sharpImg = (file, outputWay, size) => new Promise( r => { sharp(file).resize(size || 800).toFile( outputWay ); r();} );
-let convertImg = (way, fileName, quality) => new Promise(r=> webp.cwebp(`${way}/src/${fileName}.jpg`,`dist/${fileName}.webp`, quality || '-q 85', (status,err) => err ? console.log(err) : r() ) );
+let convertImg = (way, fileName, quality) => new Promise(r=> webp.cwebp(`${way}/src/${fileName}`,`dist/${fileName}.webp`, quality || '-q 85', (status,err) => err ? console.log(err) : r() ) );
 
 
 (async ()=>{
@@ -18,17 +18,18 @@ let convertImg = (way, fileName, quality) => new Promise(r=> webp.cwebp(`${way}/
     let files = await promReadDir(`${way}/src`);
         
     for (let file of files) {
-            let fileName = file.replace(/(\.(jpg|png)$|\s)/g, '');
+            const extension = '.' + file.match(/\..+/)[0];
+            let fileName = file.replace(/(\.(jpg|png)$|\s)/, '');
             let fileSize = await promFileStat(`${way}/src/${file}`);
 
             let imgSize = process.argv.includes(/s{1}\d+/g);
             if (imgSize) imgSize = imgSize[0];
             
-            await sharpImg(`${way}/src/${file}`, `${way}/dist/${fileName}.jpg`, imgSize);
+            // await sharpImg(`${way}/src/${file}`, `${way}/dist/${fileName}.jpg`, imgSize);
 
             let quality = process.argv.includes(/q{1}\d+/g);
             if (quality) quality = quality[0];
 
-            await convertImg(way, fileName, quality);
+            await convertImg(way, fileName + extension, quality);
     }
 })();
